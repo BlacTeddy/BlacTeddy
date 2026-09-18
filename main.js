f660cd658f309790752f20c53ea958c8e653528c
BlacTeddy <drogan87@gmail.com>
2026-09-18 10:23:52 -0400

    Update main.js
    re-add JS from HTML

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
-/*
-<script>
-    const words = [
-      'skills',
-      'projects',
-      'toward my first role'
-    ];
+const words = [
+  'skills',
+  'projects',
+  'toward my first role'
+];
 
-    let index = 0;
-    const el = document.getElementById('rotating-word');
+let index = 0;
+const el = document.getElementById('rotating-word');
 
-    setInterval(() => {
+function rotateWord(){
+    // fade out
+    el.classList.remove('show');
+    
+    setTimeout(() => {
       index = (index + 1) % words.length;
       el.textContent = words[index];
-    }, 2000); // change every 2 seconds
-  </script>
-*/
+    
+      // fade in
+      el.classList.add('show');
+    }, 600); // match transition time
+}
+
+setInterval(rotateWord, 3500);// change every 2 seconds
